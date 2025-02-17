b132949ff3a91c69bacad822b592297047e16901
MbiluM <mabilu@usf.edu>
2025-02-16 19:37:49 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,13 @@ function calculateSalary(baseSalary, bonus, taxRate){
 };
 console.log(calculateSalary(5000, 500, 0.1)); // output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // output: "Net Salary: $6950.00"
+
+
+// Task 2 - Product Price After Discount
+function calculateDiscount(price, discountRate){
+    finalPrice = price - (price * discountRate)
+    return `Final Price: $${finalPrice.toFixed(2)}`
+};
+console.log(calculateDiscount(100, 0.2)); // output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // output: "Final Price: $212.50"
+
